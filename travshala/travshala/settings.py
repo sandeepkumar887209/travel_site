fff72e80f12dc6e11eb86ea9316ff52ff7d57984
Sandeep Kumar <sandeepkumar887209@gmail.com>
2025-09-11 09:28:44 +0530

    Update settings.py

--- travshala/travshala/settings.py
+++ travshala/travshala/settings.py
@@ -131,7 +131,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'er.sandeep307@gmail.com'
+EMAIL_HOST_USER = '@gmail.com'
 EMAIL_HOST_PASSWORD = 'pdpg wxxd aplp sjtr'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
